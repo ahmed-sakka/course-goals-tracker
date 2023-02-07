2a628b10f3b598b6249249ab0739d9c5a24d80b3
Ahmed Sakka <ahmedsakka720@gmail.com>
2023-02-07 16:21:44 +0100

    using CSS modules

--- src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './CourseGoalItem.css';
+import classes from './CourseGoalItem.module.css';
 
 const CourseGoalItem = props => {
   // const [deleteText, setDeleteText] = useState('');
@@ -11,7 +11,7 @@ const CourseGoalItem = props => {
   };
 
   return (
-    <li className="goal-item" onClick={deleteHandler}>
+    <li className={classes["goal-item"]} onClick={deleteHandler}>
       {props.children}
     </li>
   );
